b1c98e5fa74e665fa3324251df0c03191127b954
EgorBYV <egorvrublevskij@gmail.com>
2021-03-04 09:48:51 +0300

    commit one

---
+++ app.js
@@ -0,0 +1,21 @@
+// 1.3 Операторы
+// в JS присутствует большое количество операторов, которые позволяют взаимодействовать с большим типом переменных
+let currentYear = 2020
+const BirthYear = 1989
+const age = currentYear - BirthYear
+
+// console.log(age)
+// в данном контексте всё, что валидно для математики тут будет работать
+const a = 10
+const b = 20
+let c = 32
+
+
+console.log(a+b)
+console.log(a-b)
+console.log(a*b)
+console.log(a/b)
+
+console.log(currentYear--) // ++ операция инкрементирование
+console.log(++currentYear)
+console.log(c)
